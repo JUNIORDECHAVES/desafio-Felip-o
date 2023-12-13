838cbb82dfb89d5f6e105f3b39fe5a93f02087c9
Junior chaves <juniorchaves0010@gmail.com>
2023-12-13 14:55:13 -0300

    update final!

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const name = "Junior";
-let xpDoHeroi = 10001;
+let xpDoHeroi = 8000;
 let nivel = " ";
 
 if (xpDoHeroi < 1000) {
     nivel = "Ferro"
 }
-else if(xpDoHeroi < 2001) {
+else if((xpDoHeroi >= 1001) && (xpDoHeroi <= 2000)) {
     nivel = "Bronze"
 }
-else if (xpDoHeroi < 6001) {
+else if ((xpDoHeroi >= 2001) && (xpDoHeroi <= 5000)) {
     nivel = "Prata"
 }
-else if (xpDoHeroi < 7001 ) {
+else if ((xpDoHeroi >= 6001) && (xpDoHeroi <= 7000)) {
     nivel = "Ouro"
 }
-else if (xpDoHeroi < 8001 ) {
+else if ((xpDoHeroi >= 7001) && (xpDoHeroi <= 8000)) {
     nivel = "Platina"
 }
-else if (xpDoHeroi < 9001) {
+else if ((xpDoHeroi >= 8001) && (xpDoHeroi <= 9000)) {
     nivel = "Ascendente"
 }
-else if (xpDoHeroi < 10001) {
+else if ((xpDoHeroi >= 9001) && (xpDoHeroi <= 10000) ) {
     nivel = "Imortal" 
 }
 else {
